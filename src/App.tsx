3ea4a001ce2b535ffeefe34f4be0e44485ce11f9
Antonio Garcia <agl0809@gmail.com>
2020-02-01 01:01:42 -0500

    feat(app): add router and basic sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,49 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { generatePin } from 'generate-pin';
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Generate PIN Manager</p>
-      </header>
+      <Router>
+        <div>
+          <nav>
+            <ul className="App-menu">
+              <li>
+                <Link to="/">Generate</Link>
+              </li>
+              <li>
+                <Link to="/saved">Saved</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/Saved">
+              <Saved />
+            </Route>
+            <Route path="/">
+              <Generate />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
+  );
+};
+
+class Generate extends React.Component {
+  pins = generatePin(5).map(pin => <p>{pin}</p>);
+  render() {
+    return <header className="App-header">{this.pins}</header>;
+  }
+}
+
+const Saved = () => {
+  return (
+    <header className="App-header">
+      <h2>Saved</h2>
+    </header>
   );
 };
 
